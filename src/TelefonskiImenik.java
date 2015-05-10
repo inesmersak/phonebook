82b44dc97c59c4a243fc21f0c6de2084a4d4adce
Ines <inesmersak@gmail.com>
2015-05-10 18:51:33 +0200

    Dodala kodo za povezavo z databazo, s pomočjo SQLite.
    Dodana je tudi datoteka z databazo, .jar file, ki je potreben za
    delovanje SQLite in JDBC. Prav tako je preimenovan glavni razred.

---
+++ src/TelefonskiImenik.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class TelefonskiImenik {
+	public static Connection poveziSeZBazo() {
+		Connection c = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:imenik.db");
+		} catch (Exception e) {
+			System.out.println("Povezava z bazo ni uspela.");
+			System.out.println(e.getClass().getName() + ": " + e.getMessage());
+		}	
+		return c;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		Connection c = poveziSeZBazo();
+		if (c == null) {
+			return ;
+		}
+	}
+}
--- src/telefonskiImenik.java
+++
@@ -1,8 +0,0 @@
-
-public class telefonskiImenik {
-
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
-
-}
